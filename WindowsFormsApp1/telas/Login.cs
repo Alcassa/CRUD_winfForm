29fb77157515ca6169a09b4369d2e4f2f8018612
Alcassa <galcassa49@gmail.com>
2023-11-23 16:56:52 -0300

    Adicionando menu de login

---
+++ WindowsFormsApp1/telas/Login.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1.telas
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtUsuario.Text.Equals("alcassa") && txtSenha.Text.Equals("123"))
+                {
+                    var menu = new MenuPrincipal();
+                    this.Visible = false;
+                    menu.Show();
+                }
+                else
+                {
+                    MessageBox.Show("usuário ou senha incorreto",
+                        "Desculpe",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Desculpe",
+                ex.Message
+                , MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
+
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    } 
+}
--- WindowsFormsApp1/telas/MenuPrincipal.Designer.cs
+++ WindowsFormsApp1/telas/MenuPrincipal.Designer.cs
@@ -39,13 +39,13 @@
             // 
             // menuStrip1
             // 
-            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.opçõesToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1233, 35);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 1, 0, 1);
+            this.menuStrip1.Size = new System.Drawing.Size(822, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -55,29 +55,28 @@
             this.adicionarCadastroToolStripMenuItem,
             this.pesquisarCadastrosToolStripMenuItem});
             this.opçõesToolStripMenuItem.Name = "opçõesToolStripMenuItem";
-            this.opçõesToolStripMenuItem.Size = new System.Drawing.Size(89, 29);
+            this.opçõesToolStripMenuItem.Size = new System.Drawing.Size(59, 22);
             this.opçõesToolStripMenuItem.Text = "Opções";
             // 
             // adicionarCadastroToolStripMenuItem
             // 
             this.adicionarCadastroToolStripMenuItem.Name = "adicionarCadastroToolStripMenuItem";
-            this.adicionarCadastroToolStripMenuItem.Size = new System.Drawing.Size(269, 34);
+            this.adicionarCadastroToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
             this.adicionarCadastroToolStripMenuItem.Text = "Adicionar cadastro";
             this.adicionarCadastroToolStripMenuItem.Click += new System.EventHandler(this.AdicionarCadastroToolStripMenuItem_Click);
             // 
             // pesquisarCadastrosToolStripMenuItem
             // 
             this.pesquisarCadastrosToolStripMenuItem.Name = "pesquisarCadastrosToolStripMenuItem";
-            this.pesquisarCadastrosToolStripMenuItem.Size = new System.Drawing.Size(269, 34);
+            this.pesquisarCadastrosToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
             this.pesquisarCadastrosToolStripMenuItem.Text = "Pesquisar cadastros";
             this.pesquisarCadastrosToolStripMenuItem.Click += new System.EventHandler(this.PesquisarCadastrosToolStripMenuItem_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(292, 302);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Location = new System.Drawing.Point(195, 196);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(112, 35);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
             this.button1.Text = "Adicionar";
             this.button1.UseVisualStyleBackColor = true;
@@ -85,10 +84,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(947, 302);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Location = new System.Drawing.Point(631, 196);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(112, 35);
+            this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 3;
             this.button2.Text = "Pesquisar";
             this.button2.UseVisualStyleBackColor = true;
@@ -96,15 +94,13 @@
             // 
             // MenuPrincipal
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1233, 614);
+            this.ClientSize = new System.Drawing.Size(822, 399);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.menuStrip1);
-            this.IsMdiContainer = true;
             this.MainMenuStrip = this.menuStrip1;
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "MenuPrincipal";
             this.Text = "Menu";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
